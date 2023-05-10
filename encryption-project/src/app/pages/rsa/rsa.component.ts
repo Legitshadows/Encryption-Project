5da5e167d8048c8265046f00bbd30dc2bd683fd3
Legitshadows <la.beltran19@info.uas.edu.mx>
2023-05-09 17:23:23 -0700

    rsa is working just need to add db history / edits

--- encryption-project/src/app/pages/rsa/rsa.component.ts
+++ encryption-project/src/app/pages/rsa/rsa.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {  privateKey, publicKey } from './config';
+import { JSEncrypt } from 'jsencrypt';
 
 @Component({
   selector: 'app-rsa',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./rsa.component.scss']
 })
 export class RsaComponent {
+  plainText: string = '';
+  cipherText: string = '';
 
+  $encrypt: any;
+
+
+  
+  ngOnInit() {
+    this.$encrypt = new JSEncrypt();
+  }
+
+  encrypt() {
+		const text = `${this.plainText}`.trim();
+
+		// 1024 位的密钥支持明文长度最大为 127
+		if (text.length > 117) {
+		} else {
+			this.$encrypt.setPublicKey(publicKey);
+			this.cipherText = this.$encrypt.encrypt(text);
+		}
+	}
+
+	decrypt() {
+		this.$encrypt.setPrivateKey(privateKey);
+		this.plainText = this.$encrypt.decrypt(this.cipherText);
+1
+		if (Object.is(this.plainText, null)) {
+		}
+	}
 }
